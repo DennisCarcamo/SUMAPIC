ee8ad050bc1814e8d6d7ec46c2a7f6f062cbba45
DennisCarcamo <dejocael@gmail.com>
2022-10-27 21:11:17 -0600

    DisplayPciture Screen layout created, ui only

--- screens/DisplayPictureScreen.js
+++ screens/DisplayPictureScreen.js
@@ -1,15 +1,82 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable, Image } from "react-native";
 import BatteryAndInternetStatusComponent from "../components/BatteryAndInternetStatus";
 
-function DisplayPictureScreen() {
+import BootstrapStyleSheet from 'react-native-bootstrap-styles';
+
+const bootstrapStyleSheet = new BootstrapStyleSheet();
+const { styles: s, constants: c } = bootstrapStyleSheet;
+
+
+function DisplayPictureScreen({ navigation, route }) {
+
+    const { capturedPicture } = route.params;
+
     return (
-        <View>
-            <BatteryAndInternetStatusComponent></BatteryAndInternetStatusComponent>
-            <Text>This is the DisplayPictureScreen Screen</Text>
+        <View style={styles.screenContainer}>
+            <View style={styles.headingContainer}>
+                <BatteryAndInternetStatusComponent></BatteryAndInternetStatusComponent>
+            </View>
+
+            <Image style={styles.pictureFrameContainer} source={{ uri: capturedPicture }}>
+
+            </Image>
+
+            <View style={styles.pictureActionsContainer}>
+                <Pressable style={styles.button} >
+                    <Text style={[s.text, s.title, styles.buttonText]}>Save Picture to Galery</Text>
+                </Pressable>
+            </View>
         </View>
     );
 }
 
 export default DisplayPictureScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    screenContainer: {
+        flex: 1,
+        backgroundColor: '#f3e5f5',
+    },
+    headingContainer: {
+        flex: 2,
+        marginHorizontal: 32,
+        flexDirection: "row",
+        justifyContent: 'space-between'
+    },
+    pictureFrameContainer: {
+        flex: 5,
+        marginVertical: 32,
+        marginHorizontal: 32,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        // borderWidth: 3,
+        backgroundColor: '#8e24aa',
+        // borderColor: 'black',
+    },
+    pictureActionsContainer: {
+        flex: 0.5,
+        marginTop: 0,
+        marginBottom: 32,
+        marginHorizontal: 32,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: '700'
+    },
+    button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        Width: '20%',
+        Height: '20%',
+        paddingVertical: 12,
+        paddingHorizontal: 32,
+        borderRadius: 4,
+        elevation: 3,
+        backgroundColor: '#8e24aa',
+    },
+});
--- screens/TakePictureScreen.js
+++ screens/TakePictureScreen.js
@@ -11,157 +11,168 @@ const { styles: s, constants: c } = bootstrapStyleSheet;
 
 function TakePictureScreen({ navigation }) {
 
-    const [type, setType] = useState(CameraType.back);
-    const [cameraPermission, setcameraPermission] = useState(false);
-    const [capturedPicture, setCapturedPicture] = useState(null);
-    const [isCameraReady, setIsCameraReady] = useState(false);
+  const [type, setType] = useState(CameraType.back);
+  const [cameraPermission, setcameraPermission] = useState(false);
+  const [capturedPicture, setCapturedPicture] = useState(null);
+  const [isCameraReady, setIsCameraReady] = useState(false);
 
-    const isFocused = useIsFocused();
-    const cameraRef = useRef();
+  const isFocused = useIsFocused();
+  const cameraRef = useRef();
 
-    function toggleCameraType() {
-        setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+  function toggleCameraType() {
+    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+  }
+
+  const onCameraReady = () => {
+    setIsCameraReady(true);
+  };
+
+  async function takePicture() {
+    if (cameraRef.current) {
+      const options = { quality: 0.5, base64: true, skipProcessing: true };
+      const data = await cameraRef.current.takePictureAsync(options);
+      const source = data.uri
+      if (source) {
+        setCapturedPicture(source);
+        await cameraRef.current.pausePreview();
+      }
     }
+  }
 
-    const onCameraReady = () => {
-        setIsCameraReady(true);
-    };
+  async function clearPicture() {
+    setCapturedPicture(null);
+    await cameraRef.current.resumePreview();
+  }
 
-    async function takePicture() {
-        if (cameraRef.current) {
-            const options = { quality: 0.5, base64: true, skipProcessing: true };
-            const data = await cameraRef.current.takePictureAsync(options);
-            setCapturedPicture(data.uri);
-            if (capturedPicture != null) {
-                await cameraRef.current.pausePreview();
-            }
-        }
+  function goToDisplayPicture() {
+    setCapturedPicture(null);
+    navigation.navigate('Display', { capturedPicture });
+  }
+
+  useEffect(() => {
+    async function getCammeraPermission() {
+      const permission = await Camera.requestCameraPermissionsAsync();
+      setcameraPermission(permission);
     }
+    getCammeraPermission();
+  }, []);
+  //
+  //   useEffect(() => {
+  //     console.log("se ejecuta siempre, despues de pintar");
+  //   });
+  //
+  //   useEffect(() => {
+  //     console.log("componentDidUpdate - increment, solo se ejecuta si la dependencia cambia");
+  //   }, [increment]);
+  //
+  //   useEffect(() => {
+  //     console.log("componentDidUpdate - decrement, solo se ejecuta si la dependencia cambia");
+  //   }, [decrement]);
+  //
+  //  useEffect(() => {
+  //   return () => {
+  //     console.log("Component unmounted, se retorna una funcion");
+  //   };
+  //  }, []);
 
-    async function clearPicture() {
-        setCapturedPicture(null);
-        await cameraRef.current.resumePreview();
-    }
+  return (
+    <>
 
-    function goToDisplayPicture() {
-        navigation.navigate('Display');
-    }
+      {
+        isFocused && <View style={styles.screenContainer}>
+          <View style={styles.headingContainer}>
+            <BatteryAndInternetStatusComponent></BatteryAndInternetStatusComponent>
+            <View style={styles.headingCameraContainer}>
+              {
+                capturedPicture != null
+                  ? <Pressable style={styles.button} onPress={clearPicture} >
+                    <Text style={[s.text, s.title, styles.buttonText]}>Clear</Text>
+                  </Pressable>
+                  : null
 
-    useEffect(() => {
-        async function getCammeraPermission() {
-            const permission = await Camera.requestCameraPermissionsAsync();
-            setcameraPermission(permission);
-        }
-        getCammeraPermission();
-    }, []);
-    //
-    //   useEffect(() => {
-    //     console.log("se ejecuta siempre, despues de pintar");
-    //   });
-    //
-    //   useEffect(() => {
-    //     console.log("componentDidUpdate - increment, solo se ejecuta si la dependencia cambia");
-    //   }, [increment]);
-    //
-    //   useEffect(() => {
-    //     console.log("componentDidUpdate - decrement, solo se ejecuta si la dependencia cambia");
-    //   }, [decrement]);
-    //
-    //  useEffect(() => {
-    //   return () => {
-    //     console.log("Component unmounted, se retorna una funcion");
-    //   };
-    //  }, []);
+              }
+            </View>
+          </View>
+          <Camera type={type} style={styles.pictureFrameContainer} ref={cameraRef} onCameraReady={onCameraReady} >
 
-    return (
-        <>
-
+          </Camera>
+          <View style={styles.pictureActionsContainer}>
+            <Pressable style={styles.button} onPress={toggleCameraType}>
+              <Text style={[s.text, s.title, styles.buttonText]}>Flip</Text>
+            </Pressable>
+            <Pressable style={styles.button} onPress={takePicture}>
+              <Text style={[s.text, s.title, styles.buttonText]}>Take Picture</Text>
+            </Pressable>
             {
-                isFocused && <View style={styles.screenContainer}>
-                    <View style={styles.headingContainer}>
-                        <BatteryAndInternetStatusComponent></BatteryAndInternetStatusComponent>
-                        <View style={styles.headingCameraContainer}>
-                            <Pressable style={styles.button} onPress={clearPicture}>
-                                <Text style={[s.text, s.title, styles.buttonText]}>Clear</Text>
-                            </Pressable>
-                        </View>
-                    </View>
-                    <Camera type={type} style={styles.pictureFrameContainer} ref={cameraRef} onCameraReady={onCameraReady} >
-
-                    </Camera>
-                    <View style={styles.pictureActionsContainer}>
-                        <Pressable style={styles.button} onPress={toggleCameraType}>
-                            <Text style={[s.text, s.title, styles.buttonText]}>Flip</Text>
-                        </Pressable>
-                        <Pressable style={styles.button} onPress={takePicture}>
-                            <Text style={[s.text, s.title, styles.buttonText]}>Take Picture</Text>
-                        </Pressable>
-                        <Pressable style={[styles.button, styles.actionButton]} disabled={capturedPicture == null} onPress={goToDisplayPicture}>
-                            <Text style={[s.text, s.title, styles.buttonText]}>Next</Text>
-                        </Pressable>
-                    </View>
-                </View>
+              capturedPicture != null
+                ? <Pressable style={[styles.button, styles.actionButton]} disabled={capturedPicture == null} onPress={goToDisplayPicture}>
+                  <Text style={[s.text, s.title, styles.buttonText]}>Next</Text>
+                </Pressable>
+                : null
             }
-        </>
-    );
+          </View>
+        </View>
+      }
+    </>
+  );
 }
 
 export default TakePictureScreen;
 
 const styles = StyleSheet.create({
-    screenContainer: {
-        flex: 1,
-        backgroundColor: '#f3e5f5',
-    },
-    headingContainer: {
-        flex: 2,
-        marginHorizontal: 32,
-        flexDirection: "row",
-        justifyContent: 'space-between'
-    },
-    headingCameraContainer: {
-        flex: 0.80,
-        flexDirection: 'column',
-        justifyContent: 'flex-end',
-        alignContent: 'center',
-    },
-    pictureFrameContainer: {
-        flex: 5,
-        marginVertical: 32,
-        marginHorizontal: 32,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        // borderWidth: 3,
-        backgroundColor: '#8e24aa',
-        // borderColor: 'black',
-    },
-    pictureActionsContainer: {
-        flex: 0.5,
-        marginTop: 0,
-        marginBottom: 32,
-        marginHorizontal: 32,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
+  screenContainer: {
+    flex: 1,
+    backgroundColor: '#f3e5f5',
+  },
+  headingContainer: {
+    flex: 2,
+    marginHorizontal: 32,
+    flexDirection: "row",
+    justifyContent: 'space-between'
+  },
+  headingCameraContainer: {
+    flex: 0.80,
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    alignContent: 'center',
+  },
+  pictureFrameContainer: {
+    flex: 5,
+    marginVertical: 32,
+    marginHorizontal: 32,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    // borderWidth: 3,
+    backgroundColor: '#8e24aa',
+    // borderColor: 'black',
+  },
+  pictureActionsContainer: {
+    flex: 0.5,
+    marginTop: 0,
+    marginBottom: 32,
+    marginHorizontal: 32,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
 
-    },
-    buttonText: {
-        color: 'white',
-        fontWeight: '700'
-    },
-    button: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        Width: '20%',
-        Height: '20%',
-        paddingVertical: 12,
-        paddingHorizontal: 32,
-        borderRadius: 4,
-        elevation: 3,
-        backgroundColor: '#8e24aa',
-    },
-    actionButton: {
-        marginHorizontal: 4
-    }
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '700'
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    Width: '20%',
+    Height: '20%',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: '#8e24aa',
+  },
+  actionButton: {
+    marginHorizontal: 4
+  }
 });
